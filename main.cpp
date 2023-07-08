5d9e5c12049bc6a3e0a3675564fb0bcbb0f0564f
18Harshil <138773799+18Harshil@users.noreply.github.com>
2023-07-08 16:56:47 +0530

    Add files via upload
    temp 3

--- main.cpp
+++ main.cpp
@@ -2,15 +2,35 @@
 
 using namespace std;
 
+// class Movie{
+// private :
+//     string rating;
+// public :
+//     string title;
+//     string director;
 
-    int main() {
- for (int i=0 ; i<100 ; i++)
- {
-        if (i%2==0){
-            continue ;}
-            cout<<i<<endl;
-        }
+//      Movie (string atitle , string adirector , string arating){
+//             title = atitle ;
+//             director = adirector ;
+//             rating = arating ;
+//             }
+//             void setrating(string arating) {
+//              if (arating == "g" || arating "pg-13"){
+//                rating = arating ;
+//              } else {
+//              rating ="NR";
+//              }
+//             }
 
+//             string getrating(){
+//             return rating ;
+//             }
+//     };
+int main()
+{
+    // Movie avanger("the avengers","Joss Whedon","PG -13")
+    // avenger.setrating("Dog");
+    cout <<"Hello "<<endl;
 
     return 0;
 }
