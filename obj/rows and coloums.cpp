08b20383faa239a4dd5431f3f6534b7007ca6a50
18Harshil <138773799+18Harshil@users.noreply.github.com>
2023-07-10 11:15:17 +0530

    Add files via upload

---
+++ obj/rows and coloums.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+    int rows,coloums;
+    cin>>rows>>coloums ;
+for(int i=1 ; i<rows ;i++)
+{for(int j=1 ; j<coloums ; j++)
+{
+    cout<<"*"  ;
+
+}cout<<endl ;
+
+}
+
+
+
+
+        return 0 ;
+}
