2c98aef03ac1f473fde39cf406a519cfad359e41
18Harshil <138773799+18Harshil@users.noreply.github.com>
2023-07-10 08:59:27 +0530

    Add files via upload

---
+++ switch problem.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   switch(button)
+   {
+   case 'a':
+    cout<<"Hello"<<endl ;
+    break;
+     case 'b' :
+    cout<<"Hell"<<endl ;
+    break;
+    case 'c' :
+    cout<<"Helo"<<endl ;
+    break;
+    case 'd' :
+    cout<<"Hllo"<<endl ;
+    break;
+
+
+
+    default:
+        cout<<"xyz"<<endl ;
+        break ;
+
+
+
+   }
+
+        return 0 ;
+
+}
